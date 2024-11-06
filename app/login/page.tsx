d37ce0994c643af6c8081c9271370f4e5a513eb6
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2024-11-06 14:56:58 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+//import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* esquerda */}
@@ -19,10 +27,13 @@ const LoginPage = () => {
           A Finance IA é uma plataforma de gestão financeira que utiliza IA para
           ajudar nas tomadas de decisões com base nos dados fornecidos por você.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* direita */}
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { dark } from "@clerk/themes";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <div className="flex w-screen flex-col items-center justify-center font-bold">
-      <h1 className="p-5">Hello Word</h1>
-      <Button>Olá mano</Button>
+      <h1 className="p-5">Hello World</h1>
+      <UserButton
+        showName
+        appearance={{
+          baseTheme: dark,
+        }}
+      />
     </div>
   );
 };
